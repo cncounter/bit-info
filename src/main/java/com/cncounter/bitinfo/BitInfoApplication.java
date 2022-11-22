9c8d2e102929a67050f87a0f79b7edb1c839b119
renfufei <renfufei@qq.com>
2022-11-22 17:12:08 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/cncounter/bitinfo/BitInfoApplication.java
@@ -0,0 +1,13 @@
+package com.cncounter.bitinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cncounter/bitinfo/BitInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cncounter.bitinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
